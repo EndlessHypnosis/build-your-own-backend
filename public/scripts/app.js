6c312607665ba554f2030793a864d008a81e1ad6
Endless Hypnosis <endlesshypnosis@gmail.com>
2017-10-10 11:14:24 -0600

    attempting to call knex from browser

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,43 @@
 
+// const knexDialect = require('knex/lib/dialects/postgres');
+
+
 // SETUP: knex
-const environment = 'test';
-const configuration = require('../../knexfile')[environment];
-const database = require('knex')(configuration);
+// const environment = 'development';
+// const configuration = require('../../knexfile')[environment];
+// configuration.client = knexDialect;
+// const database = require('knex')(configuration);
+
+
+
+const pg = require('pg');
+
+
+// let knex = require('knex');
+const knexDialect = require('knex/lib/dialects/postgres');
+
+const knexConfig = {
+  client: 'pg',
+  connection: 'postgres://localhost/byob',
+  migrations: {
+    directory: '../../db/migrations'
+  },
+  seeds: {
+    directory: '../../db/seeds/test'
+  },
+  useNullAsDefault: true
+}
+
+knexConfig.client = knexDialect;
+
+const database = require('knex')(knexConfig);
+
+// bookshelf(knex);
+
+
+
+
+
 
 
 
@@ -16,35 +51,40 @@ const fillData = () => {
 }
 
 const migrateLatest = () => {
-  database.migrate.latest();
+  console.log('what is database:', database);
+  
+  database.migrate.latest()
+  .then(() => {
+    console.log('done migrating');
+  })
 }
 
 const migrateRollback = () => {
   database.migrate.rollback();
 }
 
-const fetchAllBreweries = () => {
-  return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`)
-    .then(results => results.json())
-    .then(movies => {
-      return cleanMovieData(movies.results)
-    })
-}
+// const fetchAllBreweries = () => {
+//   return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`)
+//     .then(results => results.json())
+//     .then(movies => {
+//       return cleanMovieData(movies.results)
+//     })
+// }
 
 
-const cleanMovieData = (movieArray) => {
-  return movieArray.map(movie => {
-    return {
-      movieId: movie.id,
-      title: movie.title,
-      releaseDate: movie.release_date,
-      description: movie.overview,
-      voteAverage: movie.vote_average,
-      posterImg: movie.poster_path,
-      isFavorited: false
-    }
-  })
-}
+// const cleanMovieData = (movieArray) => {
+//   return movieArray.map(movie => {
+//     return {
+//       movieId: movie.id,
+//       title: movie.title,
+//       releaseDate: movie.release_date,
+//       description: movie.overview,
+//       voteAverage: movie.vote_average,
+//       posterImg: movie.poster_path,
+//       isFavorited: false
+//     }
+//   })
+// }
 
 
 
